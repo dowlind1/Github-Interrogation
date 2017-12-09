c972420c45925ce8d0c8c74222cc218a98f184a4
Daniel Dowling <32266502+dowlind1@users.noreply.github.com>
2017-12-09 21:55:20 +0000

    Search Box function
    This will collect the character input at each point from the user.
    I used the console.log() to test the code was working while I was displaying it. Through web expression I would upload a temp website. I plan on displaying on my server in TCD

--- githubapi.js
+++ githubapi.js
@@ -1,1 +1,14 @@
 ﻿
+$(document).ready(function(){
+		console.log('Connected');//sends a message on our console to let us know it is working, for testing purposes
+		$('#searchBox').on('keyup', function(input){
+			console.log('character entry');//used to test that for each time the user inputs a character to the search box, it will appear on console
+			console.log('input.target.value');//this will give the value of the text entered at each point
+			let userLogin = input.target.value;
+    
+    
+    }
+}
+      
+      
+      
