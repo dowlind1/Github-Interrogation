e7b36979b7c0c0a74245d0b64bd4e1584c6119f2
Daniel Dowling <32266502+dowlind1@users.noreply.github.com>
2017-12-10 21:51:58 +0000

    Includinged AJAX for API request
    This is my own client id and details. Cannot publish public or will need correction. If the API requests run out please use your own OAuth to keep it working. 
    Started to create the functions which will be defining the API requests, and allowing to call on more details

--- githubapi.js
+++ githubapi.js
@@ -1,13 +1,24 @@
 ﻿
 $(document).ready(function(){
-		console.log('Connected');//sends a message on our console to let us know it is working, for testing purposes
-		$('#searchBox').on('keyup', function(input){
-			console.log('character entry');//used to test that for each time the user inputs a character to the search box, it will appear on console
-			console.log('input.target.value');//this will give the value of the text entered at each point
-			let userLogin = input.target.value;
-    
-    
-    }
+	console.log('Connected');//sends a message on our console to let us know it is working, for testing purposes
+	$('#searchBox').on('keyup', function(input){
+		console.log('character entry');//used to test that for each time the user inputs a character to the search box, it will appear on console
+		console.log('input.target.value');//this will give the value of the text entered at each point
+		let userLogin = input.target.value;
+    		
+		//Use ajax to get a request from the Github API
+		$.ajax({
+			url:'https://api.github.com/users/'+userLogin,
+			//will only get about 50 requests per hour with out an Oauth code, provided by Github
+			//These are my own details: dowlind1
+			data:{
+				client_id:'6199e68ea54464745137',
+				client_secret:'e79ecbcec7e674422e563fa4984c6e06cb612066'
+			}
+		}).done(function(userDetails) {//returns details, which is the user details
+			console.log(userDetails);
+		}
+	}
 }
       
       
