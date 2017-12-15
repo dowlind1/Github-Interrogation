97a59c2d4c0e5c9f87c0d77580f47547f8f6fdfd
Daniel Dowling <32266502+dowlind1@users.noreply.github.com>
2017-12-15 13:43:33 +0000

    Testing code, error found
    And corrected

--- githubapi.js
+++ githubapi.js
@@ -105,9 +105,9 @@ $(document).ready(function(){
 				</div>
 				<h3 class="page-header">Public Repositories:</h3>
 				<div id="userRepos"></div>
-				var userVar = userLogin;
-				localStorage.setItem("userVar",userVar):
 			`);
+			var userVar = userLogin;
+			localStorage.setItem("userVar",userVar):
 		});
 	});
 });
